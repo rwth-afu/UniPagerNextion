9a41787e66c5e4a4d47db96c50faae7b8e63ff32
Ralf Wilke DH3WR <ralfwil@gmx.de>
2017-09-05 21:34:42 +0200

    Move to bash and added some info

--- src/install.sh
+++ src/install.sh
@@ -1,10 +1,17 @@
-#!/bin/sh -x
-
+#!/bin/bash 
+echo "Must be run as root!"
 cp UniPagerNextion.py /usr/local/bin/unipagernextion.py
 chmod 744 /usr/local/bin/unipagernextion.py
 cp unipagernextion.py.example /etc/unipagernextionconfig.py
 chmod 744 /etc/unipagernextionconfig.py
 mkdir -p /usr/local/lib/systemd/system
 cp unipagernextion.service /usr/local/lib/systemd/system/unipagernextion.service
-
 systemctl daemon-reload
+
+echo "Now edit /etc/unipagernextionconfig.py according to your needs."
+echo "To start unipagernextion type:"
+echo "systemctl start unipagernextion"
+
+echo "To enable unipagernextion at every boot type:"
+echo "systemctl enable unipagernextion"
+
